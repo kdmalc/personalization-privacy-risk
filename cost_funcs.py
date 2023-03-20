Fixed grad (normD off), corrected Hessian

cost_funcs.py
@@ -1,34 +1,11 @@
 import numpy as np
 
-# For np.tensordot(a, b)
-#axes = 0 : tensor product 
-#axes = 1 : tensor dot product 
-#axes = 2 : (default) tensor double contraction 
-
-def hessian_cost_1_2identity(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((2*alphaE*(F@F.T).T + 2*alphaD*np.identity(F.shape[0]) ).flatten())
-def hessian_cost_1identity_2identity(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((2*alphaE*(F@F.T).T@np.identity(F.shape[0]) + 2*alphaD*np.identity(F.shape[0]) ).flatten())
-def hessian_cost_1_2ones(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((2*alphaE*(F@F.T).T + 2*alphaD*np.ones(F.shape) ).flatten())
-def hessian_cost_1ones_2ones(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((2*alphaE*(F@F.T).T@np.ones(F.shape) + 2*alphaD*np.ones(F.shape) ).flatten())
-def hessian_cost_1_2tpidentity(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((2*alphaE*(F@F.T).T + 2*alphaD*np.identity(F.shape[0]) ).flatten())
-def hessian_cost_1tpidentity_2tpidentity(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((np.tensordot((2*alphaE*(F@F.T).T), np.identity(F.shape[0])) + np.tensordot(2*alphaD, np.identity(F.shape[0])) ).flatten())
-def hessian_cost_1_2tpones(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((2*alphaE*(F@F.T).T + 2*alphaD*np.ones(F.shape) ).flatten())
-def hessian_cost_1tpones_2tpones(F, alphaD, alphaE=1e-6):
-    # These are technically supposed to be multipled (tensor product) with Identity 
-    return ((np.tensordot((2*alphaE*(F@F.T).T), np.ones(F.shape)) + np.tensordot(2*alphaD, np.ones(F.shape)) ).flatten())
+# np.kron(a, b) is the kronecker product
+# The function assumes that the number of dimensions of a and b are the same, if necessary prepending the smallest with ones. If a.shape = (r0,r1,..,rN) and b.shape = (s0,s1,...,sN), the Kronecker product has shape (r0*s0, r1*s1, ..., rN*SN) --> Thus for 2D: output is (r0*s0, r1*s1) --> In our case, must have square output since F@F.T is square and identity is square
+def hessian_cost_l2(F, alphaD, alphaE=1e-6):
+    # Not sure about shape indices... should they all be 0? Or m, n, m like it is now?  
+    # Dims work out in both cases...
+    return 2*alphaE*np.kron((F@F.T), np.identity(F.shape[1])) + 2*alphaD*np.kron(np.identity(F.shape[0]), np.identity(F.shape[1]))
 
 
 # set up gradient of cost: