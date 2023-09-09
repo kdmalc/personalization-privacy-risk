cea2d2949c069ff96655db08cd93984c21a55e0f
kdmalc <kdmalc@gmail.com>
2023-09-09 10:22:58 -0500

    Full 112 client run!

--- Personalized_Federated_Learning/run_commands/test_commands.sh
+++ Personalized_Federated_Learning/run_commands/test_commands.sh
@@ -6,7 +6,7 @@ python -u main.py -tr_ids ['METACPHS_S106'] -con_num [1,2,3,4,5,6,7,8] -gr 100 -
 # From my stat hetero assignment earlier, Clients 2,4,9,10,12 were the closest group --> Results in 40 clients LOL
 python -u main.py -tr_ids "['METACPHS_S108', 'METACPHS_S110', 'METACPHS_S115', 'METACPHS_S116', 'METACPHS_S118']" -con_num [1,2,3,4,5,6,7,8] -gr 100 -lr 1 -lrt 25 -lD 0.00001 -lE 0.000001 -algo FedAvg
 python -u main.py -tr_ids "['METACPHS_S108', 'METACPHS_S110', 'METACPHS_S115', 'METACPHS_S116', 'METACPHS_S118']" -con_num [1,2,3,4,5,6,7,8] -gr 100 -lr 1 -lrt 25 -lD 0.00001 -lE 0.000001 -algo Local
-
+  
 # Now run FedAvg but with different numbers of clients to see if the resulting global model is better with more clients (and if the stat hetero I saw is fake news or not)
 
 # ALL CLIENTS RUN!!!
