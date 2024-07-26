Fixed bug around averaging test loss too many times, but test still better than train...

Personalized_Federated_Learning/flcore/clients/clientavg.py
@@ -7,6 +7,9 @@ import time
 from flcore.clients.clientbase import Client
 from flcore.pflniid_utils.privacy import *
 
+from sklearn.decomposition import PCA
+from utils.processing_funcs import normalize_tensor
+
 
 class clientAVG(Client):
     def __init__(self, args, ID, samples_path, labels_path, condition_number, **kwargs):
@@ -14,18 +17,8 @@ class clientAVG(Client):
 
     def train(self):
         trainloader = self.load_train_data()
-        # self.model.to(self.device)
-        #self.model.train()
-
-        # differential privacy
-        #if self.privacy:
-        #    self.model, self.optimizer, trainloader, privacy_engine = \
-        #        initialize_dp(self.model, self.optimizer, trainloader, self.dp_sigma)
-        
+        self.model.train()
         start_time = time.time()
-        #if self.train_slow:
-        #    max_local_steps = np.random.randint(1, max_local_steps // 2)
-
         if self.verbose:
             print(f'Client {self.ID} Training')
         # Save the client's starting weights for Smoothbatch
@@ -40,13 +33,64 @@ class clientAVG(Client):
                 for i, (x, y) in enumerate(trainloader):
                     if self.verbose:
                         print(f"Epoch {epoch}, grad step {step}, batch {i}")
-                    self.cphs_training_subroutine(x, y)
-        # Do SmoothBatch if applicable
-        if self.smoothbatch_boolean:
-            with torch.no_grad():
-                for name, param in self.model.named_parameters():
-                    if param.requires_grad:
-                        param.data = self.smoothbatch_learningrate*starting_weights[name] + (1 - self.smoothbatch_learningrate)*param.data
+                    #self.cphs_training_subroutine(x, y)
+                    self.optimizer.zero_grad()
+                    self.model.train() # Does this need to be here...
+                    #loss_obj, num_samples = self.shared_loss_calc(x, y, self.model, train_mode=True)
+                    #F, V, y_ref = self.simulate_data_streaming_xy(x, y, input_model=model, train_mode=train_mode)
+                    s_temp = x
+                    p_reference = y
+                    if self.normalize_data:
+                        s_normed = normalize_tensor(s_temp)
+                        p_reference = normalize_tensor(p_reference)
+                    else:
+                        s_normed = s_temp
+                    if self.pca_channels!=self.device_channels:
+                        pca = PCA(n_components=self.pca_channels)
+                        s = torch.tensor(pca.fit_transform(s_normed), dtype=torch.float32)
+                    else:
+                        s = s_normed
+                    F = s[:-1,:]
+                    v_actual = self.model(s)
+                    p_actual = torch.cumsum(v_actual, dim=1)*self.dt
+                    #V = (p_reference - p_actual)*self.dt
+                    y_ref = p_reference[:-1, :]  # To match the input
+                    #return F, V, y_ref    
+                    vel_pred = self.model(F)
+                    l2_loss = sum(torch.norm(param, p=2)**2 for name, param in self.model.named_parameters() if 'weight' in name)
+                    t1 = self.loss_func(vel_pred, y_ref)
+                    t2 = self.lambdaD*(l2_loss)
+                    t3 = self.lambdaF*(torch.linalg.matrix_norm((F))**2)
+                    self.cost_func_comps_log = [(t1.item(), t2.item(), t3.item())]
+                    loss_obj = t1 + t2 + t3 #+ reg_sum
+                    num_samples = x.size()[0]
+                    #return loss, num_samples
+                    loss_obj.backward()
+                    self.loss_log.append(loss_obj.item()/num_samples)
+                    if self.model_str == 'LinearRegression':
+                        weight_grad = self.model.weight.grad
+                        if weight_grad == None:
+                            print("Weight gradient is None...")
+                            grad_norm = -1
+                            self.gradient_norm_log.append(grad_norm)
+                        else:
+                            grad_norm = torch.linalg.norm(self.model.weight.grad, ord='fro') 
+                            self.gradient_norm_log.append(grad_norm)
+                    else:
+                        print(f"{self.model_str} is not LinearRegression: running separate grad norm extraction")
+                        grad_norm = 0
+                        for param in self.model.parameters():
+                            if param.grad is not None:
+                                param_norm = param.grad.data.norm(2)
+                                grad_norm += param_norm.item() ** 2
+                        grad_norm = grad_norm ** 0.5
+                        self.gradient_norm_log.append(grad_norm)
+                    self.optimizer.step()
+                    if self.smoothbatch_boolean:
+                        with torch.no_grad():
+                            for name, param in self.model.named_parameters():
+                                if param.requires_grad:
+                                    param.data = self.smoothbatch_learningrate*starting_weights[name] + (1 - self.smoothbatch_learningrate)*param.data
 
         if self.learning_rate_decay:
             self.learning_rate_scheduler.step()

Personalized_Federated_Learning/flcore/clients/clientbase.py
@@ -110,7 +110,7 @@ class Client(object):
         self.ndp = args.num_decimal_points
         
         self.loss_func = CPHSLoss(lambdaF=self.lambdaF, lambdaD=self.lambdaD, lambdaE=self.lambdaE)
-        # This is the training loss log written to during cphs_subrountine
+        # This is the training loss log written to during CPHS_TRAINING_SUBROUTINE
         self.loss_log = []
         # Testing loss log written to directly within client.test_metrics()
         self.client_testing_log = []
@@ -302,10 +302,7 @@ class Client(object):
                 v_actual = input_model(s)
         # Numerical integration of v_actual to get p_actual
         p_actual = torch.cumsum(v_actual, dim=1)*self.dt
-        # I don't think I actually use V later on
-        ## Well it's used in the cost function... and gradient I believe...
-        ### Wait does this code not even use the gradient...
-        V = (p_reference - p_actual)*self.dt
+        V = (p_reference - p_actual)*self.dt # V doesn't actually get used...
         y_ref = p_reference[:-1, :]  # To match the input
         #^ This is used in t1 = self.loss_func(vel_pred, self.y_ref) in shared_loss_calc
         return F, V, y_ref
@@ -482,6 +479,7 @@ class Client(object):
             eval_model = self.load_model(saved_model_path)
         else:
             # USING THE CLIENT'S LOCAL(/PERS) MODEL, NOT THE GLOBAL MODEL!
+            ## This should be the default case, right?
             eval_model = self.model
         eval_model.to(self.device)
         eval_model.eval()
@@ -497,23 +495,26 @@ class Client(object):
                 else:
                     x = x.to(self.device)
                 y = y.to(self.device)
-
+                
                 loss, num_samples = self.shared_loss_calc(x, y, eval_model, train_mode=False)
                 # I think an issue could be if after streaming the values are getting "double-dipped"
-                ## Eg the training values of F, V, y_ref are geting used for testing as well...?
-                ### Or rather the computational graph (gradient/history) remains and is what is getting double-dipped...
+                ## F, y_ref are geting used for testing as well, or rather the computational graph (gradient/history) remains and is what is getting double-dipped...
+                print(f"(loss, num_samples): ({loss}, {num_samples})")
 
                 total_loss += loss.item() * num_samples
                 total_samples += num_samples
-
+            print(f"TOTAL (total_loss, total_samples): ({total_loss}, {total_samples})")
             average_loss = total_loss / total_samples
             self.client_testing_log.append(average_loss)
-        return average_loss, total_samples
+            print(f"CALC'd AVERAGE: {average_loss}\n")
+        return total_loss, total_samples # Return average_loss or total_loss...
     
 
     def train_metrics(self, saved_model_path=None, model_obj=None):
         '''Kai's docs: This function is for evaluating the model (on the training data for some reason) during training
-        Note that model.eval() is called so params aren't updated.'''
+        Note that model.eval() is called so params aren't updated.
+        
+        7/26/24: Why does this exist, just use the loss during actual training??? This is how PFL broke it up tho...'''
         if self.verbose:
             print("Client train_metrics()")
 
@@ -538,6 +539,7 @@ class Client(object):
             print(f'cb Client {self.ID} train_metrics()')
         with torch.no_grad():
             for i, (x, y) in enumerate(trainloader):
+                print(f'cb Client {self.ID} train_metrics() batch {i}')
                 if type(x) == type([]):
                     x[0] = x[0].to(self.device)
                 else:
@@ -546,9 +548,10 @@ class Client(object):
 
                 #print(f"TRAIN_METRICS, USER {self.ID}")
                 loss, num_samples = self.shared_loss_calc(x, y, eval_model)
+                print(f"(loss, num_samples): ({loss}, {num_samples})")
                 train_num += num_samples
                 losses += loss.item() * num_samples
-
+        print(f"TOTAL (losses, train_num): ({losses}, {train_num})\n")
         return losses, train_num
 
     